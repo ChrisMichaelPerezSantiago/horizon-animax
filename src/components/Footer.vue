97f96df146e03c35eae21f388515bce703347c07
Chris Michael <chris.perez@upr.edu>
2019-08-17 21:42:03 -0400

    The NavBar and Footer components were created

---
+++ src/components/Footer.vue
@@ -0,0 +1,34 @@
+<template>
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 text-center">
+          
+        </div>
+        <div class="col-md-6 text-center">   
+         
+        </div>
+      </div>
+      <div class="text-center bottom">
+        <span>© {{year}} Horizon Animax , Todos los derechos reservados</span>
+      </div>
+    </div>
+  </footer>
+</template>
+
+
+<script>
+  import { value } from 'vue-function-api';
+
+  export default {
+    setup(){
+      const values = {
+        year: new Date().getFullYear()
+      };
+
+      return{
+        ...values
+      }
+    }
+  }
+</script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-nav">
+    <div class="container">
+      <a class="navbar-brand" href="#">
+        
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#"><i class="fas fa-home"></i> Reciente</a>
+          </li>
+           <li class="nav-item">
+            <a class="nav-link" href="#"><i class="fas fa-ticket-alt"></i> películas</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"><i class="fas fa-ticket-alt"></i> Ovas</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"><i class="fas fa-sort-alpha-down"></i> por Alfabeto</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"><i class="fas fa-ellipsis-h"></i> por géneros</a>
+          </li>
+        </ul>
+        <div class="search-bar">
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Buscar Anime ..." aria-label="Search">
+            <button class="btn btn-main my-2 my-sm-0" type="submit"><i class="fas fa-search"></i></button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
