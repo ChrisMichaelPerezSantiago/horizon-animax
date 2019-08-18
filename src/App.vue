9c7ee84bf58b50f812b558000c02b1939b34d444
Chris Michael <chris.perez@upr.edu>
2019-08-17 21:39:21 -0400

    Footer and NavBar components added in the src/App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,23 @@
 <template>
   <div id="app">
+    <NavBar/>
     <router-view />
+    <Footer/>
   </div>
 </template>
 
 <style>
 </style>
+
+<script>
+  import NavBar from "./components/NavBar";
+  import Footer from "./components/Footer"
+
+  export default {
+    components:{
+      NavBar,
+      Footer
+    }
+  }
+</script>
+
