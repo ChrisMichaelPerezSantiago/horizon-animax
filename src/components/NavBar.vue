047e425e65de63e496607d0a9ce2f7a22fd5ca49
Chris Michael <chris.perez@upr.edu>
2019-08-19 19:11:16 -0400

    nav bar collapse now works perfectly

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,14 +1,14 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-nav">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-nav" v-bind:class="{'navbarOpen': show }">
     <div class="container">
       <a class="navbar-brand" href="#">
         
       </a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" v-on:click.prevent="toggleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <div class="collapse navbar-collapse" id="navbarSupportedContent" v-bind:class="{'show': show }">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item">
             <a class="nav-link" href="#"><i class="fas fa-home"></i> Reciente</a>
@@ -36,3 +36,21 @@
     </div>
   </nav>
 </template>
+
+
+<script>
+  import {value} from 'vue-function-api';
+
+  export default {
+    name: "NavBar",
+    setup(){
+      let show = value(true);
+      const toggleNavbar = () => show.value = !show.value;      
+        
+      return{
+        show,
+        toggleNavbar
+      }
+    }
+  }
+</script>
