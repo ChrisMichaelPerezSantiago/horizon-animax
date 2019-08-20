ebf2cb97d01b2a605fb60519acfc2d8cdf46a883
Chris Michael <chris.perez@upr.edu>
2019-08-19 21:43:10 -0400

    The LetterBox.vue component was created

---
+++ src/components/LetterBox.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div id="letrasbg"></div>
+    <div id="letrasint" class="letras-int">
+      <div class="letras-box">
+        <ul>
+          <li><a class="letra-link" href="/letter/A/" title="Animes por letra A">A</a></li>
+          <li><a class="letra-link" href="/letter/B/" title="Animes por letra B">B</a></li>
+          <li><a class="letra-link" href="/letter/C/" title="Animes por letra C">C</a></li>
+          <li><a class="letra-link" href="/letter/D/" title="Animes por letra D">D</a></li>
+          <li><a class="letra-link" href="/letter/E/" title="Animes por letra E">E</a></li>
+          <li><a class="letra-link" href="/letter/F/" title="Animes por letra F">F</a></li>
+          <li><a class="letra-link" href="/letter/G/" title="Animes por letra G">G</a></li>
+          <li><a class="letra-link" href="/letter/H/" title="Animes por letra H">H</a></li>
+          <li><a class="letra-link" href="/letter/I/" title="Animes por letra I">I</a></li>
+          <li><a class="letra-link" href="/letter/J/" title="Animes por letra J">J</a></li>
+          <li><a class="letra-link" href="/letter/K/" title="Animes por letra K">K</a></li>
+          <li><a class="letra-link" href="/letter/L/" title="Animes por letra L">L</a></li>
+          <li><a class="letra-link" href="/letter/M/" title="Animes por letra M">M</a></li>
+          <li><a class="letra-link" href="/letter/N/" title="Animes por letra N">N</a></li>
+          <li><a class="letra-link" href="/letter/O/" title="Animes por letra O">O</a></li>
+          <li><a class="letra-link" href="/letter/P/" title="Animes por letra P">P</a></li>
+          <li><a class="letra-link" href="/letter/Q/" title="Animes por letra Q">Q</a></li>
+          <li><a class="letra-link" href="/letter/R/" title="Animes por letra R">R</a></li>
+          <li><a class="letra-link" href="/letter/S/" title="Animes por letra S">S</a></li>
+          <li><a class="letra-link" href="/letter/T/" title="Animes por letra T">T</a></li>
+          <li><a class="letra-link" href="/letter/U/" title="Animes por letra U">U</a></li>
+          <li><a class="letra-link" href="/letter/V/" title="Animes por letra V">V</a></li>
+          <li><a class="letra-link" href="/letter/W/" title="Animes por letra W">W</a></li>
+          <li><a class="letra-link" href="/letter/X/" title="Animes por letra X">X</a></li>
+          <li><a class="letra-link" href="/letter/Y/" title="Animes por letra Y">Y</a></li>
+          <li><a class="letra-link" href="/letter/Z/" title="Animes por letra Z">Z</a></li>
+          <li class="box-link tp"><a href="#tipoint" title="Animes por Tipo">Por Tipo &darr;</a></li>
+          <li class="box-link lt"><a href="#letrasint" title="Animes por Genero">Por Genero &darr;</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
