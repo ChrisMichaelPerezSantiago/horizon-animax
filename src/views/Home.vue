22dafdd43a62a3f2be10de5632ce12225a30548c
Chris Michael <chris.perez@upr.edu>
2019-08-21 10:51:22 -0400

    The Anime.vue component is in use

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,34 +1,43 @@
 <template>
   <div class="container">
     <LetterBox/> <!-- LETTER BOX -->
-    <div>
-
-    </div>
+    <main class="Main">
+      <div class="">
+        <h2>Últimas series</h2>
+      </div>
+      <ul class="ListEpisodios AX Rows A06 C04 D03">
+        <li v-for="(anime, index) in latestAnimes" :key="index">
+          <Anime :animes="anime"/>
+        </li>
+      </ul>
+    </main>
   </div>
 </template>
 
 <script>
-  import {onCreated} from 'vue-function-api';
-  import {useState , useStore} from '@u3u/vue-hooks';
-  import Anime from '../components/Anime';
-  import LetterBox from '../components/LetterBox';
-  export default {
-    name: "home",
-    components:{
-      Anime,
-      LetterBox
-    },
-    setup(){
-      const store = useStore();
-      const state = {
-        ...useState(['latestAnimes' , 'isLoading'])
-      };
-      onCreated(() =>{
-        store.value.dispatch('GET_LATEST_ANIME')
-      });
-      return{
-        ...state
-      }
-    }
-  };
+import { onCreated } from "vue-function-api";
+import { useState, useStore } from "@u3u/vue-hooks";
+import Anime from "../components/Anime";
+import LetterBox from "../components/LetterBox";
+
+export default {
+  name: "home",
+  components: {
+    Anime,
+    LetterBox
+  },
+  setup() {
+    const store = useStore();
+    const state = {
+      ...useState(["latestAnimes", "isLoading"])
+    };
+    onCreated(() => {
+      store.value.dispatch("GET_LATEST_ANIME");
+    });
+
+    return {
+      ...state
+    };
+  }
+};
 </script>
