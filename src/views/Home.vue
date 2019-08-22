33dbb102bec4ebf5c9e167c6d152c1f1db9a44ad
Chris Michael <chris.perez@upr.edu>
2019-08-22 18:12:29 -0400

    minor changes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -20,27 +20,27 @@
 </template>
 
 <script>
-import { onCreated } from "vue-function-api";
-import { useState, useStore } from "@u3u/vue-hooks";
-import Anime from "../components/Anime";
-import LetterBox from "../components/LetterBox";
-export default {
-  name: "home",
-  components: {
-    Anime,
-    LetterBox
-  },
-  setup() {
-    const store = useStore();
-    const state = {
-      ...useState(["latestAnimes", "isLoading"])
-    };
-    onCreated(() => {
-      store.value.dispatch("GET_LATEST_ANIME");
-    });
-    return {
-      ...state
-    };
-  }
-};
+  import { onCreated } from "vue-function-api";
+  import { useState, useStore } from "@u3u/vue-hooks";
+  import Anime from "../components/Anime";
+  import LetterBox from "../components/LetterBox";
+  export default {
+    name: "home",
+    components: {
+      Anime,
+      LetterBox
+    },
+    setup() {
+      const store = useStore();
+      const state = {
+        ...useState(["latestAnimes", "isLoading"])
+      };
+      onCreated(() => {
+        store.value.dispatch("GET_LATEST_ANIME");
+      });
+      return {
+        ...state
+      };
+    }
+  };
 </script>
